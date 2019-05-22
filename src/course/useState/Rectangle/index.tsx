f1295fe328b9f7668f12528f3cb0996167898259
yangbo <1832064870@qq.com>
2019-05-22 16:53:38 +0800

    update

--- src/course/useState/Rectangle/index.tsx
+++ src/course/useState/Rectangle/index.tsx
@@ -3,27 +3,27 @@ import { Slider } from 'antd-mobile';
 import './index.scss';
 
 export default function Rectangle() {
-  const [width, setWidth] = useState(10);
   const [height, setHeight] = useState(10);
+  const [width, setWidth] = useState(10);
 
   const style = {
-    width: `${width}px`,
-    height: `${height}px`
+    height: `${height}px`,
+    width: `${width}px`
   }
 
   return (
     <div className="container">
-      <p>width:</p>
-      <Slider
-        max={300}
-        min={10}
-        onChange={(n) => setWidth(n || 0)}
-      />
       <p>height:</p>
       <Slider
         max={300}
         min={10}
         onChange={(n) => setHeight(n || 0)}
+      />
+      <p>width:</p>
+      <Slider
+        max={300}
+        min={10}
+        onChange={(n) => setWidth(n || 0)}
       />
       <div className="reatangle" style={style} />
     </div>
