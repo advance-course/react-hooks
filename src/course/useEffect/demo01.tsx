14b0929884b199e8880df3f7483e787d10492df7
yangbo <1832064870@qq.com>
2019-05-29 20:54:16 +0800

    useEffect demo

--- src/course/useEffect/demo01.tsx
+++ src/course/useEffect/demo01.tsx
@@ -1,8 +1,54 @@
-import React, {} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import anime from 'animejs';
+import './style.scss';
 
 export default function AnimateDemo() {
+  const [anime01, setAnime01] = useState(false);
+  const [anime02, setAnime02] = useState(false);
+  const element = useRef<any>();
+
+  useEffect(() => {
+    anime01 && !anime02 && animate01();
+    anime02 && !anime01 && animate02();
+  }, [anime01, anime02]);
+
+  function animate01() {
+    if (element) {
+      anime({
+        targets: element.current,
+        translateX: 400,
+        backgroundColor: '#FF8F42',
+        borderRadius: ['0%', '50%'],
+        complete: () => {
+          setAnime01(false);
+        }
+      })
+    }
+  }
+
+  function animate02() {
+    if (element) {
+      anime({
+        targets: element.current,
+        translateX: 0,
+        backgroundColor: '#FFF',
+        borderRadius: ['50%', '0%'],
+        easing: 'easeInOutQuad',
+        complete: () => {
+          setAnime02(false);
+        }
+      })
+    }
+  }
+
+  function clickHandler() {
+    setAnime01(true);
+    setTimeout(setAnime02.bind(null, true), 500);
+  }
+
   return (
-    <div>hello</div>
+    <div className="container" onClick={clickHandler}>
+      <div className="el" ref={element} />
+    </div>
   )
 }
